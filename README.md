dc6df7f451da976905b67cd7b19f6fb7e4461389
m-tahaali <tahaali.6074+github@gmail.com>
2023-11-16 22:33:00 +0500

    Installed Libraries

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-# 12 Bead 
+# 12 Bead
+
+## Prerequisites
+### [SFML](https://www.sfml-dev.org/)
+- Add SFML bin Folder in System Path (or but contents in folder with exe file.)
+### [Websocketpp](https://github.com/zaphoyd/websocketpp)
+### [Asio (Standalone)](https://github.com/chriskohlhoff/asio)
+### [MinGW Std Threads](https://github.com/meganz/mingw-std-threads)
+- Folder name as mingw-threads
+
+## Compiling GUI (MinGW)
+### Windows
+```sh
+del main.exe *.o
+g++ -I "<sfml folder>/include" -c main.cpp
+g++ -I "<Home Folder with websocketpp and mingw-threads>" -I "<asio folder>/include" -c server/socket.cpp
+g++ -c server/game.cpp
+g++ game.o main.o -o main -L "<sfml folder>/lib" -lsfml-graphics -lsfml-window -lsfml-system -lws2_32 -lwsock32
+main
+```
 
 ## Compiling consoleBased (MinGW)
 ### Linux
---
+++ server/socket.cpp
@@ -0,0 +1,48 @@
+#define ASIO_STANDALONE
+#define _WEBSOCKETPP_MINGW_THREAD_
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
+
+#include <functional>
+#include <iostream>
+
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
+
+void on_message(websocketpp::server<websocketpp::config::asio>* s, websocketpp::connection_hdl hdl, websocketpp::server<websocketpp::config::asio>::message_ptr msg) {
+    std::cout << "on_message called with hdl: " << hdl.lock().get()
+              << " and message: " << msg->get_payload()
+              << std::endl;
+
+    try {
+        s->send(hdl, msg->get_payload(), msg->get_opcode());
+    } catch (websocketpp::exception const & e) {
+        std::cout << "Echo failed because: "
+                  << "(" << e.what() << ")" << std::endl;
+    }
+}
+
+const int port = 3000;
+
+websocketpp::server<websocketpp::config::asio> endpoint;
+
+void init_server() {
+    endpoint.set_access_channels(websocketpp::log::alevel::all);
+    endpoint.clear_access_channels(websocketpp::log::alevel::frame_payload);
+
+    endpoint.init_asio();
+}
+
+void run_server() {
+    endpoint.listen(port);
+    endpoint.set_message_handler(bind(&on_message,&endpoint,::_1,::_2));
+    endpoint.start_accept();
+    endpoint.run();
+}
+
+int main() {
+    init_server();
+    run_server();
+    return 0;
+}
